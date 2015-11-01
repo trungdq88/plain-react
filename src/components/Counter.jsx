eaf28cf81ccf50becacc427c6ebe6f7c6c21062f
Dinh Quang Trung <trungdq88@gmail.com>
2015-11-01 23:34:14 +0700

    :pencil: Fix lint

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -5,7 +5,7 @@ import React, { Component, PropTypes } from 'react';
 
 class Counter extends Component {
   render() {
-    const { increment, incrementIfOdd, incrementAsync, decrement, counter } = this.props
+    const { increment, incrementIfOdd, incrementAsync, decrement, counter } = this.props;
     return (
       <p>
         Clicked: {counter} times
